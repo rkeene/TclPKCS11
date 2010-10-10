3ed0fd99e50ba0214ba46b011a3de18c51c7c0b6
rkeene <rkeene@noemail.net>
2010-10-10 04:45:32 +0000

    Added signing and verification to test
    Made test less verbose
    
    FossilOrigin-Name: 0a81f17bc947224b0bedb18c35aab2568387f295c199a3a1daa9cb0ac027a203

--- test.tcl
+++ test.tcl
@@ -2,14 +2,14 @@
 
 lappend auto_path [file join [pwd] work lib]
 
-set pkcs11_module "/usr/local/lib/libcackey_g.so"
+set pkcs11_module "/usr/local/lib/libcackey.so"
 
 load tclpkcs11.so Tclpkcs11
 
-set handle [::pki::pkcs11::loadmodule $pkcs11_module]
+set handle [pki::pkcs11::loadmodule $pkcs11_module]
 puts "Handle: $handle"
 
-set slots [::pki::pkcs11::listslots $handle]
+set slots [pki::pkcs11::listslots $handle]
 puts "Slots: $slots"
 
 foreach slotinfo $slots {
@@ -29,16 +29,19 @@ if {![info exists token_slotid]} {
 	exit 1
 }
 
-set certs [::pki::pkcs11::listcerts $handle $token_slotid]
+set certs [pki::pkcs11::listcerts $handle $token_slotid]
 puts "Found [llength $certs] certificates"
-set orig "TestMsg"
-foreach certinfo $certs {
-	puts "Cert: $certinfo"
 
-	set cipher [pki::encrypt -binary -pub $orig $certinfo]
+set orig "TestMsg"
+foreach certinfo_list $certs {
+	unset -nocomplain certinfo
+	array set certinfo $certinfo_list
+	puts "Cert: $certinfo(pkcs11_label) / $certinfo(subject)"
+
+	set cipher [pki::encrypt -binary -pub $orig $certinfo_list]
 
 	if {[catch {
-		set plain  [pki::decrypt -binary -priv $cipher $certinfo]
+		set plain  [pki::decrypt -binary -priv $cipher $certinfo_list]
 	} err]} {
 		if {$err == "PKCS11_ERROR USER_NOT_LOGGED_IN"} {
 			# Login and try it again...
@@ -46,22 +49,29 @@ foreach certinfo $certs {
 			flush stdout
 
 			gets stdin password
-			::pki::pkcs11::login $handle $token_slotid $password
+			pki::pkcs11::login $handle $token_slotid $password
 
-			set plain  [pki::decrypt -binary -priv $cipher $certinfo]
+			set plain  [pki::decrypt -binary -priv $cipher $certinfo_list]
 		}
 	}
 
 	if {$plain != $orig} {
 		puts "Decryption error!  Expected \"$orig\", got \"$plain\""
 
-		exit
+		exit 1
 	}
 
-	set cipher [pki::encrypt -binary -priv $orig $certinfo]
-	set plain  [pki::decrypt -binary -pub $cipher $certinfo]
+	set cipher [pki::encrypt -binary -priv $orig $certinfo_list]
+	set plain  [pki::decrypt -binary -pub $cipher $certinfo_list]
 
-	puts "Got Match!"
+	set sig    [pki::sign $orig $certinfo_list]
+	set verify [pki::verify $sig $orig $certinfo_list]
+
+	if {!$verify} {
+		puts "Signature verification error!"
+
+		exit 1
+	}
 }
 
-::pki::pkcs11::unloadmodule $handle
+pki::pkcs11::unloadmodule $handle
